a61e81849236de83350cb1fbe4807bbb8221540e
Nailton Sousa <nailton@fordev.com.br>
2015-07-31 22:34:43 -0300

    [UPDATE] - criado dois recursos Projects e Tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', 'WelcomeController@index');
+
+Route::resource('projects', 'ProjectsController');
+// Route::resource('tasks', 'TasksController');
+Route::resource('projects.tasks', 'TasksController');
+
+Route::bind('tasks', function($value, $route){
+  return App\Task::whereSlug($value)->first();
+});
+Route::bind('projects', function($value, $route){
+  return App\Project::whereSlug($value)->first();
 });
